c7228d2b29b483939017141c4cd40526aa8e05cd
Your Name <you@example.com>
2016-07-29 12:03:46 +0300

    изменено:      install.sh 	новый файл:    install_x86_64.sh 	изменено:      listpackages 	новый файл:    listpackages_x86_64 	новый файл:    rpmlist 	изменено:      steamrun

--- install.sh
+++ install.sh
@@ -27,7 +27,8 @@ if ! [ -d  "tar"  ]; then
 
                         if ! [ -f deb/$(echo  $BIN_DISTFILES | rev | sed -r 's/\/.+//' | rev) ]; then
          
-                       cd  deb &&   fetch $MASTER_SITES$BIN_DISTFILES  && cd ../
+                       cd  deb &&   fetch $MASTER_SITES$BIN_DISTFILES
+                       cd ../
  
                         fi 
                  done
@@ -67,19 +68,47 @@ cp       tar/libflashsupport.so            ubuntu/usr/lib
 ln -s    libudev.so.1.3.5                  ubuntu/lib/i386-linux-gnu/libudev.so.0
 ln -s    libcurl.so.4.3.0                  ubuntu/usr/lib/i386-linux-gnu/libcurl.so.5 
 ln -s    bash                              ubuntu/bin/sh
-cp -R    certs                             ubuntu/etc/ssl
 
 
-cp /compat/linux/usr/lib/$(ls /compat/linux/usr/lib/ | grep libGL.so | head -2 | tail -n 1) ubuntu/usr/lib
-cp /compat/linux/usr/lib/$(ls /compat/linux/usr/lib/ | grep libnvidia-glcore) ubuntu/usr/lib
-cp /compat/linux/usr/lib/$(ls /compat/linux/usr/lib/ | grep libnvidia-tls) ubuntu/usr/lib
+  if ! [ -z "$(dmesg | grep radeon)" ] ; then 
 
-ln -s  $(ls /compat/linux/usr/lib/ | grep libGL.so | head -2 | tail -n 1)              ubuntu/usr/lib/libGL.so.1
+        echo 'RADEON'
 
+   
+              if ! [ -f "tar/linux-c6-dri-11.0.7.txz" ]; then 
+
+                      cd tar &&  fetch   http://pkg.freebsd.org/freebsd:11:x86:64/latest/All/linux-c6-dri-11.0.7.txz && cd ../
+
+               fi
+
+                if ! [ -f "tar/mesa-private-llvm-3.6.2-1.el6.i686.rpm" ]; then 
+
+                     cd tar && fetch ftp://195.220.108.108/linux/centos/6.8/os/i386/Packages/mesa-private-llvm-3.6.2-1.el6.i686.rpm
+                     cd ..
+                  fi
+
+              cd tar &&  rpm2cpio.pl   mesa-private-llvm-3.6.2-1.el6.i686.rpm | cpio -idmv 
+          cd ..
+          cp tar/usr/lib/libLLVM-3.6-mesa.so               ubuntu/usr/lib
+
+
+                 tar xf tar/linux-c6-dri-11.0.7.txz   -C    tar  
+                 cp -R tar/compat/linux/usr/lib             ubuntu/usr
+                 ln -s libtxc_dxtn_s2tc.so.0                ubuntu/usr/lib/i386-linux-gnu/libtxc_dxtn.so 
+
+        else
+
+                 cp /compat/linux/usr/lib/$(ls /compat/linux/usr/lib/ | grep libGL.so | head -2 | tail -n 1) ubuntu/usr/lib
+                 cp /compat/linux/usr/lib/$(ls /compat/linux/usr/lib/ | grep libnvidia-glcore) ubuntu/usr/lib
+                 cp /compat/linux/usr/lib/$(ls /compat/linux/usr/lib/ | grep libnvidia-tls) ubuntu/usr/lib
+                 ln -s  $(ls /compat/linux/usr/lib/ | grep libGL.so | head -2 | tail -n 1)              ubuntu/usr/lib/libGL.so.1
+
+   fi 
  
-       if ! [ -f "tar/linux-skype_oss_wrapper-0.1.1.txz" ]; then 
 
-     cd tar && fetch http://pkg.freebsd.org/freebsd:11:x86:64/latest/All/linux-skype_oss_wrapper-0.1.1.txz && cd ../
+      if ! [ -f "tar/linux-skype_oss_wrapper-0.1.1.txz" ]; then 
+
+         cd tar && fetch http://pkg.freebsd.org/freebsd:11:x86:64/latest/All/linux-skype_oss_wrapper-0.1.1.txz && cd ../
 
         fi
 
@@ -88,18 +117,22 @@ ln -s  $(ls /compat/linux/usr/lib/ | grep libGL.so | head -2 | tail -n 1)       
 
 
 
-sudo chroot ubuntu /usr/lib/i386-linux-gnu/gdk-pixbuf-2.0/gdk-pixbuf-query-loaders \
+
+doas chroot ubuntu /usr/lib/i386-linux-gnu/gdk-pixbuf-2.0/gdk-pixbuf-query-loaders \
  >  ubuntu/usr/lib/i386-linux-gnu/gdk-pixbuf-2.0/2.10.0/loaders.cache
 
-du -a /compat/ubuntu/usr/share/ca-certificates | sed 's/\/compat\/ubuntu\/usr\/share\/ca-certificates\///' |  awk '{print $2}' \
+du -a ubuntu/usr/share/ca-certificates | sed 's/ubuntu\/usr\/share\/ca-certificates\///' |  awk '{print $2}' \
  >>  ubuntu/etc/ca-certificates.conf
 
-sudo cp -R  ubuntu /compat
-sudo chroot /compat/ubuntu locale-gen en_US.UTF-8
-sudo chroot /compat/ubuntu locale-gen ru_RU.UTF-8
-sudo chroot /compat/ubuntu /bin/dbus-uuidgen --ensure
 
-sudo mkdir  /compat/ubuntu/tmp
-sudo chroot /compat/ubuntu update-ca-certificates
-sudo chroot /compat/ubuntu update-ca-certificates
+doas cp -R  ubuntu /compat
+doas chroot /compat/ubuntu locale-gen en_US.UTF-8
+doas chroot /compat/ubuntu locale-gen ru_RU.UTF-8
+doas chroot /compat/ubuntu /bin/dbus-uuidgen --ensure
+
+
+doas mkdir  /compat/ubuntu/tmp
+doas chroot /compat/ubuntu update-ca-certificates
+doas chroot /compat/ubuntu update-ca-certificates
+
 
